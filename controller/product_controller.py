bdcbe97ffea96c2729db5c4548263fd2d894f4e7
ThanhHoang02 <101106973+ThanhHoang02@users.noreply.github.com>
2024-04-17 13:37:35 +0700

    Handle search/filter products

--- controller/product_controller.py
+++ controller/product_controller.py
@@ -3,6 +3,10 @@ from model.product_model import ProductModel
 product_model = ProductModel()
 
 
-@app.route("/products/all")
+@app.route("/products/all", methods=["GET"])
 def get_all_products():
     return product_model.get_all_products()
+
+@app.route("/products/search", methods=["GET"])
+def search_products():
+    return product_model.search_products()
