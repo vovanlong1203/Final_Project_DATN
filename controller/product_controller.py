5dc8ff19fd536dac67ad7ce138b8c8f97beb10f2
ThanhHoang02 <101106973+ThanhHoang02@users.noreply.github.com>
2024-04-20 10:28:42 +0700

    handle get product detail

--- controller/product_controller.py
+++ controller/product_controller.py
@@ -10,3 +10,7 @@ def get_all_products():
 @app.route("/products/search", methods=["GET"])
 def search_products():
     return product_model.search_products()
+
+@app.route("/products/product_detail", methods=["GET"])
+def get_product_detail():
+    return product_model.get_product_detail()
